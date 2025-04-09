7db5a5cd27d7b102803e0458de18ed0560cf3a35
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-09 15:38:32 +0500

    add urls for model Recipient

--- mailing_service/urls.py
+++ mailing_service/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 
 from .views import base
 from mailing_service.apps import MailingServiceConfig
+from .views import RecipientCreateView, RecipientListView, RecipientDetailView, RecipientUpdateView, RecipientDeleteView
 
 app_name = MailingServiceConfig.name
 
 urlpatterns =[
-    path("", base, name="base"),
+    path("", base, name="base"),  # надо поменять на home
+    path("recipients/", RecipientListView.as_view(), name="recipient_list"),
+    path("recipients/new/", RecipientCreateView.as_view(), name="recipient_create"),
+    path("recipients/<int:pk>/", RecipientDetailView.as_view(), name="recipient_detail"),
+    path("recipients/<int:pk>/update/", RecipientUpdateView.as_view(), name="recipient_update"),
+    path("recipients/<int:pk>/delete/", RecipientDeleteView.as_view(), name="recipient_delete")
+
 ]
