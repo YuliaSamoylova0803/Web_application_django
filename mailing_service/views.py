9808c3168713df4d2c8b92418d6cd93648e7b908
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-04-10 12:39:51 +0500

    add views for Message and Mailing

--- mailing_service/views.py
+++ mailing_service/views.py
@@ -57,3 +57,82 @@ class RecipientUpdateView(UpdateView):
 class RecipientDeleteView(DeleteView):
     model = Recipient
     success_url = reverse_lazy("mailing_service:recipient_list")
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageDetailView(DetailView):
+    model = Message
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    fields = ["subject_message", "message_body", "attachment"]
+    template_name = "mailing_service/message_form.html"
+    success_url = reverse_lazy("mailing_service:message_list")
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        response.context_data["error_message"] = "Please correct the errors below"
+
+        return  response
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    fields = ["subject_message", "message_body", "attachment"]
+    template_name = "mailing_service/message_form.html"
+    success_url = reverse_lazy("mailing_service:message_list")
+
+    def get_success_url(self):
+        return reverse("mailing_service:message_detail", args=[self.kwargs.get("pk")])
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy("mailing_service:message_list.html")
+
+
+class MailingListView(ListView):
+    model = Mailing
+
+
+class MailingDetailView(DeleteView):
+    model = Mailing
+
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    fields = ["message", "status", "is_active", "first_shipment", "end_shipment", "recipients"]
+    template_name = "mailing_service/mailing_form.html"
+    success_url = reverse_lazy("mailing_service:mailing_list")
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        response.context_data["error_message"] = "Please correct the errors below"
+
+        return response
+
+
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    fields = ["message", "status", "is_active", "first_shipment", "end_shipment", "recipients"]
+    template_name = "mailing_service/mailing_form.html"
+    success_url = reverse_lazy("mailing_service:mailing_list")
+
+    def get_success_url(self):
+        return reverse("mailing_service:mailing_detail", args=[self.kwargs.get("pk")])
+
+
+class MailingDeleteView(DeleteView):
+    model = Mailing
+    success_url = reverse_lazy("mailing_service:mailing_list.html")
