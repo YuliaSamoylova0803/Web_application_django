08a1b7d68ecf6ac4d4d0adee8fc19e453b68e5f3
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-05 12:15:10 +0500

    update forms in users for register

--- users/forms.py
+++ users/forms.py
@@ -11,16 +11,16 @@ class StyleFormMixin:
         super().__init__(*args, **kwargs)
         for fild_name, fild in self.fields.items():
             if isinstance(fild, BooleanField):
-                fild.witget.attrs["class"] = "form-check-input"
+                fild.widget.attrs["class"] = "form-check-input"
             else:
-                fild.witget.attrs["class"] = "form-control"
+                fild.widget.attrs["class"] = "form-control"
 
 
 class UserRegisterForm(StyleFormMixin, UserCreationForm):
     model = User
     phone_number = forms.CharField(max_length=15, required=False,
                                    help_text="Необязательное поле: введите ваш номер телефона.")
-    username = forms.CharField(max_length=50, required=True)
+
     avatar = forms.ImageField(required=False, help_text="Изображение должно быть квадратным (рекомендуемый размер 200x200)")
     # usable_password = None
     password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput, help_text="Ваш пароль должен содержать как минимум 8 символов.")
@@ -29,7 +29,7 @@ class UserRegisterForm(StyleFormMixin, UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("avatar", "email", "username", "phone_number")
+        fields = ("avatar", "email", "phone_number")
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data.get("phone_number")
