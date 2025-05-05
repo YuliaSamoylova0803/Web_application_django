fd1b4fdcb64fd9b79ef54659435e29992d11552f
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-05 09:54:02 +0500

    add urls in users for login

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,8 @@ from users.apps import UsersConfig
 app_name = UsersConfig.name
 
 urlpatterns =[
-    path("login/", LoginView.as_view(template_name="login.html"), name="login"),  # надо поменять на home
+    path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),  # надо поменять на home
+    path
 
 
 
