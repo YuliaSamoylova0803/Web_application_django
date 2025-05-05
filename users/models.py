78b7393ef8a3b1c9f6185da055ed76252288a4f3
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-05 15:31:40 +0500

    update model User: add token

--- users/models.py
+++ users/models.py
@@ -11,6 +11,8 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to="users/avatars/", verbose_name="Аватарка", blank=True, null=True, help_text="Загрузите свою аватарку")
     country = models.CharField(max_length=50, blank=True, null=True, help_text="Введите страну проживания")
 
+    token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
