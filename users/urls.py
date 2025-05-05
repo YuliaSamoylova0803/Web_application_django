3eadac1021ba1f1cc9b0dba0d04f5567c901fc5d
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-05 12:14:31 +0500

    update urls in users for register

--- users/urls.py
+++ users/urls.py
@@ -2,15 +2,14 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
+from users.views import RegisterView
 
 app_name = UsersConfig.name
 
 urlpatterns =[
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),  # надо поменять на home
     path("logout/", LogoutView.as_view(next_page="mailing_service:base"), name="logout"),
-
-
-
+    path("register/", RegisterView.as_view(), name="register"),
 
 
 ]
