74647e8f3c52a4526a0e854fc9cef9d7394d4464
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-06 23:56:12 +0500

    update urls in users

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
     PasswordResetConfirmView, PasswordResetCompleteView
-from django.urls import path
+from django.urls import path, reverse_lazy
 import uuid
 
 from users.apps import UsersConfig
@@ -14,9 +14,36 @@ urlpatterns =[
     path("register/", RegisterView.as_view(), name="register"),
     path("email_confirm/<str:token>/", email_verification, name="email_confirm"),
 
-    path("password_reset/", PasswordResetView.as_view(), name="password_reset"),
-    path("password_reset/done/", PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path("reset/done/", PasswordResetCompleteView.as_view(), name="password_reset_complete"),
-
+    # Password reset URLs
+    path(
+        "password_reset/",
+        PasswordResetView.as_view(
+            template_name="users/password_reset_form.html",
+            email_template_name="users/password_reset_email.html",
+            success_url=reverse_lazy("users:password_reset_done"),
+            extra_email_context={
+                'protocol': 'http',
+                'domain': '127.0.0.1:8000'
+            }
+        ),
+        name="password_reset"
+    ),
+    path(
+        "password_reset/done/",
+        PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+        name="password_reset_done"
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html",
+            success_url=reverse_lazy("users:password_reset_complete")
+        ),
+        name="password_reset_confirm"
+    ),
+    path(
+        "reset/done/",
+        PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+        name="password_reset_complete"
+    ),
 ]
