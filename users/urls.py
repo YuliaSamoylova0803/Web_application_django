bbab5903202f8d43e79d40aab03558a573a021ad
Yulia Samoylova <u.u.samoylova@gmail.com>
2025-05-11 11:49:19 +0500

    update setting in config

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
     PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path, reverse_lazy
